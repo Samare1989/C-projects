989a95e41d1e630c95fb8f3b028a93b2a679be1f
sammy <samrawit.amare@ymail.com>
2019-01-29 20:56:43 -0500

    this is first c# code.. helloworld

---
+++ hellowworldsolution/hellowworldprojet/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+// everything to end of line is a comment
+namespace hellowworldprojet
+{
+
+
+
+
+
+
+    class Program
+    {
+        static int a = 7;
+        static void Print()
+        {
+            Console.WriteLine($"The value of a is {a}");
+
+        }
+
+
+        static void WhileExample()
+        {
+            var counter = 0;
+            while (counter < 10)
+            {
+                Console.WriteLine($"Count is {counter}");
+                counter = counter + 1;
+
+                // this program is the Hello,world!
+            }
+        }
+        static void Main(string[] args)
+
+
+        {
+            var counter = "0";
+
+            var b = 13;
+            var c = a * b;
+            System.Console.WriteLine("Hello,World!");
+            Console.WriteLine("by Sammy");
+            Console.WriteLine($"for a = {a} and b = {b}, a * b = {c}");
+            Console.WriteLine($"Value of counter is { counter}");
+
+
+        }
+    }
+}
